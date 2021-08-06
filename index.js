2764fb677e020221b76d0d08318fb3e16f6fc6aa
Martell Howze <martellhowze042988@gmail.com>
2021-08-05 23:39:06 -0500

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,55 @@
+function add() {
+    return (a + b);
+}
+
+function subtract() {
+    return (a - b);
+}
+
+function multiply() {
+    return (a * b);
+}
+
+function divide() {
+    return (a / b);
+}
+let x= add(4, 3);
+function add(a, b) {
+    return (a + b);
+}
+
+let y= subtract(4, 3);
+function subtract(b, a) {
+    return (b - a);
+}
+
+let w= multiply(4, 3);
+function multiply(a, b) {
+    return (a * b);
+}
+
+let z= divide(4, 3);
+function divide(a, b){
+    return (a / b);
+}
+
+let n= 5;
+function increment(n){
+   n++;
+    return n;
+}
+
+function decrement(n){
+    n--;
+    return n;
+}
+ 
+function makeInt(n){
+     
+    return parseInt(n, 10);
+     
+ }
+
+ function preserveDecimal(n){
+     return parseFloat(n);
+ }
